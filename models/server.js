c375c0210eb557a4ba40e3f18c2c1fcc14dc65bb
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-10-14 17:03:16 -0500

    ENPOINTS PARA CARGA DE IMAGENES

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { dbConnection } = require("../database/config");
+const fileUpload = require("express-fileupload");
 
 class Server {
   constructor() {
@@ -38,6 +39,15 @@ class Server {
 
     //Directorio publico
     this.app.use(express.static("public"));
+
+    //FileUpload o carga de archivos
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+        createParentPath: true,
+      })
+    );
   }
 
   routes() {
